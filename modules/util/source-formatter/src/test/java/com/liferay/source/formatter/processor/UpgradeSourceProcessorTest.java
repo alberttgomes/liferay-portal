6dcec7496e136f153d0815968804bbdb29772da6
Albert Gomes <albert.gomes@liferay.com>
2023-10-13 09:21:31 -0300

    LPS-191860 Create test to UpgradeJavaFetchCommerceAccountGroupByExternalReferenceCodeCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -246,6 +246,20 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeJavaFetchCommerceAccountGroupByExternalReferenceCodeCheck()
+		throws Exception {
+
+		test(
+			"upgrade/UpgradeJavaFetchCommerceAccountGroupByExternal" +
+			"ReferenceCodeCheck.testjava",
+			StringBundler.concat(
+				"Unable to format method ",
+				"fetchCommerceAccountGroupByExternalReferenceCode from ",
+				"CommerceAccountGroupLocalService, Fill the new changes ",
+				"manually, see TICKET."));
+	}
+
+	@Test
 	public void testUpgradeJavaFetchCPDefinitionByCProductExternalReferenceCodeCheck()
 		throws Exception {
 
